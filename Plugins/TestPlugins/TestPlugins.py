787df3505eaa2eb6a8cdffbd98c85347d6dda787
just_monika <1664382962@qq.com>
2024-04-28 00:36:58 +0800

    完善注释，新增配置加载器（未完成）

--- Plugins/TestPlugins/TestPlugins.py
+++ Plugins/TestPlugins/TestPlugins.py
@@ -1,5 +1,9 @@
-class TestPlugins:
+from Plugins import Plugins
+
+
+class TestPlugins(Plugins):
     def __init__(self, server_address):
+        super().__init__(server_address)
         self.name = "TestPlugins"
         self.type = "Group"
 
