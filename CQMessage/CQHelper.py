baa590b0a556d41b2b46abdd58daa74d0a735d6c
just_monika <1664382962@qq.com>
2024-05-18 21:35:02 +0800

    新增对CQ消息段的封装

---
+++ CQMessage/CQHelper.py
@@ -0,0 +1,87 @@
+import re
+import types
+from typing import Optional, Any, List
+
+from CQMessage.CQType import At, Image
+
+
+class CQHelper:
+    @classmethod
+    def load_cq(cls, message: str) -> Optional[Any]:
+        """
+        动态的封装 一个 记载CQ消息段的所有参数的对象
+        :param message: 要实例化的CQ消息段
+        :return: 一个实例化对象，可以直接取出其中的成员变量
+        """
+        # 匹配消息中的类型和属性
+        cq_pattern = re.compile(r'\[CQ:(\w+),([\w=,\/.]+)\]')
+        match = cq_pattern.search(message)
+
+        if not match:
+            return None
+
+        cq_type = match.group(1)
+        attrs = match.group(2)
+
+        # 创建一个动态类
+        class_name = f"{cq_type.capitalize()}"
+        dynamic_class = types.new_class(class_name)
+
+        # 解析属性并设置到动态类实例上
+        instance = dynamic_class()
+        setattr(instance, 'cq_type', cq_type)
+
+        for attr in attrs.split(','):
+            key, value = attr.split('=')
+            setattr(instance, key, value)
+
+        return instance
+
+    @classmethod
+    def loads_cq(cls, message: str) -> List[Any]:
+        """
+
+        :param message:
+        :return:
+        """
+        cq_pattern = re.compile(r'\[CQ:(\w+),([\w=,\/.]+)\]')
+        matches = cq_pattern.findall(message)
+
+        cq_objects = []
+        for match in matches:
+            cq_msg = f"[CQ:{match[0]},{match[1]}]"
+            cq_obj = cls.load_cq(cq_msg)
+            if cq_obj:
+                cq_objects.append(cq_obj)
+
+        return cq_objects
+
+
+if __name__ == "__main__":
+    # 示例
+    msg1 = "[CQ:at,qq=12345]"
+    msg2 = "[CQ:image,file=xx/xx]"
+    msg3 = "123[CQ:image,file=xx/xx]321"
+    msg4 = "123[CQ321]"
+
+    obj1: At = CQHelper.load_cq(msg1)
+    obj2: Image = CQHelper.load_cq(msg2)
+    obj3: Image = CQHelper.load_cq(msg3)
+    obj4 = CQHelper.load_cq(msg4)
+
+    print(obj1.cq_type)  # at
+    print(obj2.__dict__)  # {'cq_type': 'image', 'file': 'xx/xx'}
+    print(obj3.file)  # xx/xx
+    print(obj4)  # None
+
+    msg5 = "123[CQ:image,file=xx/xx]321[CQ:at,qq=12345]456"
+    msg6 = "123[]ewq[]231"
+
+    obj5 = CQHelper.loads_cq(msg5)
+    obj6 = CQHelper.loads_cq(msg6)
+
+    for obj in obj5:
+        print(obj.__dict__)
+        # {'cq_type': 'image', 'file': 'xx/xx'}, {'cq_type': 'at', 'qq': '12345'}
+    for obj in obj6:
+        print(obj)  # 什么都没有
---
+++ CQMessage/CQType.py
@@ -0,0 +1,79 @@
+class CQMessage:
+    def __init__(self):
+        self.cq_type = self.__class__.__name__.lower()
+
+    def __str__(self):
+        attrs = [f'{key}={value}' for key, value in self.__dict__.items() if not (key == 'cq_type' or value is None)]
+        return f'[CQ:{self.cq_type},{",".join(attrs)}]'
+
+
+class Face(CQMessage):
+    def __init__(self, id):
+        """
+        CQ:face 小黄脸表情
+        :param id: 表情的id
+        """
+        super().__init__()
+        self.id = id
+
+
+class Image(CQMessage):
+    def __init__(self, file, type=None, url=None, cache=1, proxy=1, timeout=None):
+        """
+        CQ:image 图片
+        :param file: 图片文件名
+        :param type: 可能的值：flash。图片类型，flash 表示闪照，无此参数表示普通图片
+        :param url: 图片 URL（从网络上获取的图片），只有收到时才有该参数，发送时该参数无效
+        :param cache: 只在通过网络 URL 发送时有效，表示是否使用已缓存的文件，默认 1
+        :param proxy: 只在通过网络 URL 发送时有效，表示是否通过代理下载文件（需通过环境变量或配置文件配置代理），默认 1
+        :param timeout: 只在通过网络 URL 发送时有效，单位秒，表示下载网络文件的超时时间，默认不超时
+
+        发送时，file 参数除了支持使用收到的图片文件名直接发送外，还支持：绝对路径（file:///），网络 URL（http://），Base64 编码（base64://）
+        """
+        super().__init__()
+        self.file = file
+        self.type = type
+        self.url = url
+        self.cache = cache
+        self.proxy = proxy
+        self.timeout = timeout
+
+
+class Record(CQMessage):
+    def __init__(self, file, magic=0, url=None, cache=1, proxy=1, timeout=None):
+        """
+        CQ:record 语音
+        :param file: 语音文件名
+        :param magic: 发送时可选，默认 0，设置为 1 表示变声
+        :param url: 语音 URL，只有收到时才有该参数，发送时该参数无效
+        :param cache:  只在通过网络 URL 发送时有效，表示是否使用已缓存的文件，默认 1
+        :param proxy: 只在通过网络 URL 发送时有效，表示是否通过代理下载文件（需通过环境变量或配置文件配置代理），默认 1
+        :param timeout: 只在通过网络 URL 发送时有效，单位秒，表示下载网络文件的超时时间，默认不超时
+
+        发送时，file 参数除了支持使用收到的视频文件名直接发送外，还支持其它形式，参考 Image类。
+        """
+        super().__init__()
+        self.file = file
+        self.magic = magic
+        self.url = url
+        self.cache = cache
+        self.proxy = proxy
+        self.timeout = timeout
+
+
+class At(CQMessage):
+    def __init__(self, qq):
+        """
+        CQ:at @某人
+        :param qq: 可能的值：QQ 号、all 。@的 QQ 号，all 表示全体成员
+        """
+        super().__init__()
+        self.qq = qq
+
+
+...
+# 更多CQ消息段等待补充
+
+if __name__ == "__main__":
+    at_msg = At(qq=12345)
+    print(at_msg)  # [CQ:at,qq=12345]
