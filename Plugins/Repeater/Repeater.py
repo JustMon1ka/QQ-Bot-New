8639b1c38de03662b1e460aa19fad67c484c3e57
kir1kochan <2750469650@qq.com>
2024-08-03 17:48:02 +0800

    更新了新的配置设置

--- Plugins/Repeater/Repeater.py
+++ Plugins/Repeater/Repeater.py
@@ -1,4 +1,4 @@
-from Event.EventHandler.PrivateMessageEventHandler import PrivateMessageEvent
+from Event.EventHandler.GroupMessageEventHandler import GroupMessageEvent
 from Logging.PrintLog import Log
 from Plugins import Plugins
 log = Log()
@@ -12,7 +12,7 @@ class Repeater(Plugins):
     """
     def __init__(self, server_address, bot):
         super().__init__(server_address, bot)
-        self.name = " Repeater"
+        self.name = "Repeater"
         self.type = "Group"
         self.author = "kiriko"
         self.introduction = """
@@ -21,9 +21,9 @@ class Repeater(Plugins):
                             """
         self.init_status()
         self.message_latest: str = ""
-        self.counts=0
+        self.counts = 0
         
-    async def main(self, event: PrivateMessageEvent, debug):
+    async def main(self, event: GroupMessageEvent, debug):
         
         enable = self.config.get("enable")
         if not enable:
@@ -35,8 +35,9 @@ class Repeater(Plugins):
         group_id = event.group_id
         effected_group: list = self.config.get("effected_group")
         threshold = int(self.config.get("threshold"))
-        ban=bool(self.config.get("ban"))
-        
+        ban = bool(self.config.get("ban"))
+        recall = bool(self.config.get("recall"))
+        for_everyone = bool(self.config.get("for_everyone"))
         if group_id not in effected_group:
             return
         
@@ -48,29 +49,37 @@ class Repeater(Plugins):
             self.counts += 1
              
         #到达阈值时正式进行插件的运行
-        if self.counts == threshold:
-               if  ban:
-                      reply_message = "请不要复读"
-                      self.counts = 0
-                      try:
-                         await self.api.groupService.delete_msg(message_id=event.message_id)
-                         await self.api.groupService.send_group_msg(group_id=group_id, message=reply_message)
-                         await self.api.groupService.set_group_ban(group_id=group_id, user_id=event.user_id,duration=10*60)
-                      except Exception as e:
-                        log.error(f"插件：{self.name}运行时出错：{e}")
-                      else:
-                        log.debug(f"插件：{self.name}运行正确，成功在{group_id}中撤回了一条消息：{event.message}", debug)
-                        log.debug(f"插件：{self.name}运行正确，成功向{group_id}发送了一条消息：{reply_message}", debug)
-                        log.debug(f"插件：{self.name}运行正确，成功将用户{event.user_id}禁言10分钟", debug)
-               else:
-                      reply_message = "请不要复读,否则可能被禁言哦"
-                      self.counts = 0
-                      try:
-                         await self.api.groupService.delete_msg(message_id=event.message_id)
-                         await self.api.groupService.send_group_msg(group_id=group_id, message=reply_message)
-                      except Exception as e:
-                        log.error(f"插件：{self.name}运行时出错：{e}")
-                      else:
-                        log.debug(f"插件：{self.name}运行正确，成功在{group_id}中撤回了一条消息：{event.message}", debug)
-                        log.debug(f"插件：{self.name}运行正确，成功向{group_id}发送了一条消息：{reply_message}", debug)
+        if self.counts >= threshold:
+            reply_message = self.config.get("normal_message")
+            card_cuts = event.card.split("-")
+            ban_time = self.config.get("ban_time")
+            ban_time_cuts = ban_time.split(":")
+            duration = int(ban_time_cuts[0])*3600 + int(ban_time_cuts[1])*60 + int(ban_time_cuts[0])
+            if len(card_cuts) == 3:
+                if card_cuts[1] == "助教":
+                    if for_everyone:
+                        reply_message = self.config.get("special_message")
+                    else:
+                        return
+            if recall:
+                try:
+                    await self.api.groupService.delete_msg(message_id=event.message_id)
+                except Exception as e:
+                    log.error(f"插件：{self.name}运行时出错：{e}")
+                else:
+                    log.debug(f"插件：{self.name}运行正确，成功在{group_id}中撤回了一条消息：{event.message}", debug)
+            if ban:
+                try:
+                    await self.api.groupService.set_group_ban(group_id=group_id, user_id=event.user_id,
+                                                          duration=duration)
+                except Exception as e:
+                    log.error(f"插件：{self.name}运行时出错：{e}")
+                else:
+                    log.debug(f"插件：{self.name}运行正确，成功将用户{event.user_id}禁言{ban_time}", debug)
+            try:
+                await self.api.groupService.send_group_msg(group_id=group_id, message=reply_message)
+            except Exception as e:
+                log.error(f"插件：{self.name}运行时出错：{e}")
+            else:
+                log.debug(f"插件：{self.name}运行正确，成功向{group_id}发送了一条消息：{reply_message}", debug)
         return
